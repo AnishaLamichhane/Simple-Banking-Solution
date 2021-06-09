2d71b9631d9c9d67376c0a9a98640ef691eb3e7a
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-06-09 17:19:10 +0545

    Transaction History

--- simpleBanking/TransactionView.swift
+++ simpleBanking/TransactionView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct TransactionView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Bank.entity(), sortDescriptors: [
+        NSSortDescriptor(keyPath: \Bank.date, ascending: false)
+    ]) var banks: FetchedResults<Bank>
+    
+    
     var body: some View {
-        Text("Transactions")
+        NavigationView{
+            List {
+                ForEach(banks, id: \.self) { bank in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("\(bank.wrappedRemarks)")
+                                .font(.headline)
+                            Text("\(bank.date ?? Date(), style: .date)")
+                                .foregroundColor(.secondary)
+                        }
+                        
+                        Spacer()
+                        Text("$\(bank.amount)")
+                    }
+                }
+                .onDelete(perform: deleteTransaction)
+            }
+            .navigationBarTitle("Transaction History", displayMode: .inline)
+            .navigationBarItems(trailing: EditButton())
+                
+        }
+    }
+    
+    func deleteTransaction(at offsets: IndexSet) {
+        for offset in offsets {
+            // find this book in our fetch request
+            let bank = banks[offset]
+            // delete it from the context
+            moc.delete(bank)
+        }
+        // save the context
+        try? moc.save()
     }
 }
 
